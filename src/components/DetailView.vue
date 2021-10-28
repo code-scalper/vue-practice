dc479880a5932ed782cfe79a741b6493f3ca9d43
Lee <tigerbaram@gmail.com>
2021-10-28 21:37:04 +0900

    first deploy

---
+++ src/components/DetailView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <h2>Detail Page</h2>
+      <button @click="closeDetail">close</button>
+      <input type="text" v-model="username" />
+      <button @click="sendData">send Data</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "detailPage",
+  data() {
+    return {
+      username: "",
+    };
+  },
+  methods: {
+    closeDetail() {
+      this.$emit("closeDetail");
+    },
+    sendData() {
+      this.$emit("sendData", this.username);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  background: #fff;
+  width: 60%;
+}
+</style>
---
+++ src/components/composition/TestComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Hello Reusability</h2>
+  <hr />
+  <ProductStatus />
+  <hr />
+  <FeeStatus />
+  <hr />
+  <SavingStatus />
+</template>
+
+<script>
+import FeeStatus from "../mixin/FeeStatus";
+import ProductStatus from "../mixin/ProductStatus";
+import SavingStatus from "../mixin/SavingStatus";
+import {} from "vue";
+export default {
+  name: "TestComponent",
+  components: { SavingStatus, ProductStatus, FeeStatus },
+};
+</script>
+
+<style></style>
